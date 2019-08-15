c5dc541d2f41d388cb1a6ff16cc4f97b92bec625
marszm <mariusz.szmer@gmail.com>
2019-08-15 16:17:54 +0200

    client for ab rest api - fetching data into table - OK

--- src/App.js
+++ src/App.js
@@ -1,54 +1,38 @@
 import React ,{Component} from 'react';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 class App extends Component {
 
-
-
-    state = {
-      data: [],
-  };
-
-  componentDidMount() {
-    fetch('http://localhost:8080/api/read')
-        .then(response => response.json())
-        .then(data => this.setState({data}));
-  }
-
-  renderTableData() {
-    return this.state.data.map((user) => {
-      const { id, firstName, secondName, phoneNumber, address, email } = user //destructuring
-      return (
-          <tr key={id}>
-            <td>{id}</td>
-            <td>{firstName}</td>
-            <td>{secondName}</td>
-            <td>{phoneNumber}</td>
-            <td>{address}</td>
-            <td>{email}</td>
-          </tr>
-      )
-    })
-  }
-
-  renderTableHeader() {
-    let header = Object.keys(this.state.data)
-    return header.map((key, index) => {
-      return <th key={index}>{key.toUpperCase()}</th>
-    })
-  }
-
-  render() {
-    return (
-        <div>
-          <h1 id='title'>LIST OF ALL ADDRESS BOOK USERS </h1>
-          <table id='students'>
-            <tbody>
-            <tr>{this.renderTableHeader()}</tr>
-            {this.renderTableData()}
-            </tbody>
-          </table>
-        </div>
-    );
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFetching: false,
+            users: []
+        };
     }
+
+        componentDidMount() {
+            fetch('http://localhost:8080/api/read')
+                .then(response => response.json())
+                .then(users => this.setState({users}));
+        }
+
+
+
+        render() {
+            return (
+                <div> ADDRESS BOOK USERS LIST.
+                    <BootstrapTable data={this.state.users}>
+                        <TableHeaderColumn isKey dataField='id' />
+                        <TableHeaderColumn dataField='firstName' />
+                        <TableHeaderColumn dataField='secondName' />
+                        <TableHeaderColumn dataField='phoneNumber' />
+                        <TableHeaderColumn dataField='address' />
+                        <TableHeaderColumn dataField='email' />
+                    </BootstrapTable>
+                    <p>{this.state.isFetching ? 'Fetching users...' : ''}</p>
+                </div>
+            );
+
+        }
 }
 export default App;
